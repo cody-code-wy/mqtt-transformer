265b927f0bf5d5a0a3f47f78e2cc7b8eca1db9c4
William Young <william@werl.me>
2021-11-22 15:24:20 -0600

    emit message

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import mqtt from 'mqtt';
 
 var client = mqtt.connect('mqtt://rabbitmq-service:1883',{clientId:'mqttjs01'});
-client.on("connect", () => { console.log('connected') });
+client.on("connect", () => {
+  console.log('connected')
+  client.publish("testtopic", "test message")
+});
 client.on("error", (e) => { console.log(`Can't connect ${e}`) });
+
