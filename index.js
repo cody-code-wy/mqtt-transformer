8ebcdd0e1ec0a8f862b7cba094527af8e582a9e2
William Young <william@werl.me>
2021-11-22 15:15:49 -0600

    Change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import mqtt from 'mqtt';
 
-var client = mqtt.connect('mqtt://rabbitmq-service',{clientId:'mqttjs01'});
+var client = mqtt.connect('mqtt://rabbitmq-service:1883',{clientId:'mqttjs01'});
 client.on("connect", () => { console.log('connected') });
 client.on("error", (e) => { console.log(`Can't connect ${e}`) });
