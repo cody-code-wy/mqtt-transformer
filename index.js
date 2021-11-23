5b463d73fd2718288216952319e221d617b471a0
William Young <william@werl.me>
2021-11-23 17:14:35 -0600

    update

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import mqtt from 'mqtt';
 
+const inTopic = `openflowx/${ process.env.EVENT_NAME || "in" }`
+const outTopic = `openflowx/${ process.env.EVENT_NAME || "out" }`
+
 var client = mqtt.connect('mqtt://rabbitmq-service:1883',{clientId:'mqttjs02'});
 client.on("connect", () => {
   console.log('connected')
-  client.subscribe("openflowx/get",{qos: 1})
+  client.subscribe(topic,{qos: 1})
 });
 client.on("error", (e) => { console.log(`Can't connect ${e}`) });
-client.on("message", (topic, message, packet) => {
-  console.log(`topic is ${topic}`);
-  console.log(`message is ${message}`);
+client.on("message", (inTopic, message, packet) => {
+  console.log(`got ${topic} message, sendng to ${outTopic}`);
+  client.publish(outTopic, message);
 })
 
