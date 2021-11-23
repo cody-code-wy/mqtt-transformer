4f39fb6cd4ff3c4408db6b1c0387885e2305d664
William Young <william@werl.me>
2021-11-23 00:35:32 -0600

    Add consumer code

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import mqtt from 'mqtt';
 var client = mqtt.connect('mqtt://rabbitmq-service:1883',{clientId:'mqttjs01'});
 client.on("connect", () => {
   console.log('connected')
-  client.publish("testtopic", "test message")
+  client.subscribe("testtopic",{qos: 1})
 });
 client.on("error", (e) => { console.log(`Can't connect ${e}`) });
+client.on("message", (topic, messsage, packet) => {
+  console.log(`topic is ${topic}`);
+  console.log(`message is ${message}`);
+})
 
