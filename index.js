d7341e190f6d438ca43b3ff432faf0fc7b08608b
William Young <william@werl.me>
2021-11-24 00:18:24 -0600

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const outTopic = `openflowx/${ process.env.OUT_EVENT_NAME || "out" }`
 var client = mqtt.connect('mqtt://rabbitmq-service:1883',{clientId:'mqttjs02'});
 client.on("connect", () => {
   console.log('connected')
-  client.subscribe(topic,{qos: 1})
+  client.subscribe(inTopic,{qos: 1})
 });
 client.on("error", (e) => { console.log(`Can't connect ${e}`) });
 client.on("message", (topic, message, packet) => {
